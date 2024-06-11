2eedcb04c42253aa7aa55074b86557f081a7e59f
Leonardo <leonardomourass2008@gmail.com>
2024-06-11 11:49:48 -0300

    Página de Login e cadastro em andamento

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const links = document.querySelectorAll('a[href^="#"]');
-  
+
   for (let link of links) {
-    link.addEventListener("click", function(event) {
+    link.addEventListener("click", function (event) {
       event.preventDefault();
       const targetId = this.getAttribute("href").substring(1);
       const targetElement = document.getElementById(targetId);
-      
+
       if (targetElement) {
         targetElement.scrollIntoView({ behavior: "smooth" });
       }
     });
   }
 });
+
+const login = document.querySelector('#logar')
+const cadastro = document.querySelector('#cadastrar')
+const container = document.querySelector('.container')
+
+cadastro.addEventListener('click', () => {
+  container.classList.add('modo-de-cadastro')
+})
+
+login.addEventListener('click', () => {
+  container.classList.remove('modo-de-cadastro')
+})
