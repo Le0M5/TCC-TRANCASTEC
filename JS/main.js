ca093d98988edcdc24af0a7221c9ef6f53093130
Leo <leonardomourass2008@gmail.com>
2024-06-13 15:02:49 -0300

    Cards criados pelo javascript para a div de id produtos, precisa de css ainda

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const links = document.querySelectorAll('a[href^="#"]');
-  
+
   for (let link of links) {
-    link.addEventListener("click", function(event) {
+    link.addEventListener("click", function (event) {
       event.preventDefault();
       const targetId = this.getAttribute("href").substring(1);
       const targetElement = document.getElementById(targetId);
-      
+
       if (targetElement) {
         targetElement.scrollIntoView({ behavior: "smooth" });
       }
     });
   }
 });
+
+const cardsData = [
+  { titulo: "Toque colorido", descricao: "Sensores táteis; Teclas com texturas e cores diferentes; Ajuste de volume; Tela digital que exibe partituras;" },
+  { titulo: "João", descricao: "Descrição do Card 2", url: "./IMG/Brinquedo-Piano-Xilofone-Colorido-Unissex-Pimpolho-4.webp" },
+  { titulo: "Dudu", descricao: "Descrição do Card 3", url: "" },
+  { titulo: "Card 4", descricao: "Descrição do Card 4", url: "" },
+  { titulo: "Card 5", descricao: "Descrição do Card 5", url: "" },
+  { titulo: "Card 6", descricao: "Descrição do Card 6", url: "" }
+];
+
+function createCards(data) {
+  const container = document.querySelector('#produtos');
+  container.innerHTML = data.map(item => `
+    <div class="card">
+    <img src=${item.url} alt="Imagem do piano inovador">
+      <h3>${item.titulo}</h3>
+      <p>${item.descricao}</p>
+    </div>
+  `).join('');
+}
+
+createCards(cardsData);
