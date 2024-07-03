700cee782af80387192531130b192f28f7af9384
Leonardo <leonardomourass2008@gmail.com>
2024-07-03 14:24:22 -0300

    Salvando tudo novamente mas com alterações

--- JS/main.js
+++ JS/main.js
@@ -85,3 +85,20 @@ function prevSlider() {
 
 btnNext.addEventListener('click', nextSlider)
 btnPrev.addEventListener('click', prevSlider)
+
+window.addEventListener('pageshow', function (event) {
+  const formulario = document.querySelector('.form');
+  formulario.reset();
+});
+
+
+function menuShow() {
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('open')) {
+    menuMobile.classList.remove('open');
+    document.querySelector('.icon').src = "../IMG/menu_white_36dp.svg";
+  } else {
+    menuMobile.classList.add('open');
+    document.querySelector('.icon').src = "../IMG/close_white_36dp.svg";
+  }
+}
