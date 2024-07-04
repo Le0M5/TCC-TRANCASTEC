0a9cfe9539dade487f10fcccc59274361aaf8273
Kauanny Fernandes <kauanny.fernandes.0803@gmail.com>
2024-07-04 12:46:30 -0300

    adicionei o chatbox e=nas outras páginas e arrumei o modal do produto e estilizei a página produto.

---
+++ JS/prod.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtém o modal
+    var modal = document.getElementById('myModal');
+  
+    // Obtém o botão que abre o modal
+    var btn = document.getElementById("openModalBtn");
+  
+    // Obtém o elemento de fechar
+    var span = document.getElementsByClassName("close")[0];
+  
+    // Quando o usuário clicar no botão, abra o modal
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+  
+    // Quando o usuário clicar no elemento de fechar, feche o modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+  
+    // Quando o usuário clicar fora do modal, feche-o
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  });
+  
