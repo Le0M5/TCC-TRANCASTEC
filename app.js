961292bc0c0e053bf72b2cb6c335309dc2c1ca2f
Leonardo <leonardomourass2008@gmail.com>
2024-07-05 12:15:12 -0300

    Arrumando novammente

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-      menuMobile.classList.remove('open');
-      document.querySelector('.icon').src = "./IMG/menu_white_36dp.svg";
-    } else {
-      menuMobile.classList.add('open');
-      document.querySelector('.icon').src = "./IMG/close_white_36dp.svg";
-    }
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('open')) {
+    menuMobile.classList.remove('open');
+    document.querySelector('.icon').src = "./IMG/menu_white_36dp.svg";
+  } else {
+    menuMobile.classList.add('open');
+    document.querySelector('.icon').src = "./IMG/close_white_36dp.svg";
   }
+}
+
+window.addEventListener('pageshow', function (event) {
+  const formulario = document.querySelector('.form');
+  formulario.reset();
+});
